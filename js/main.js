4df3e3174e89612d3ee0a3faae875aec52134c6d
Ozkhan Mamedov <ozhanm96@yandex.ru>
2019-06-11 20:09:12 +0300

    Правки по трейвису

--- js/main.js
+++ js/main.js
@@ -1,18 +1,20 @@
+'use strict'
+
 /**
  * Функция генерирования случайного числа
  * @param {number} min
  * @param {number} max
  * @return {number} результат генерации
  */
-var getRandomNumber = function(min, max) {
+var getRandomNumber = function (min, max) {
   return Math.round(Math.random() * (max - min) + min);
-}
+};
 
 /**
  * Функция генерирования массива объектов, описывающих объявления
  * @return {Object} результат генерации
  */
-var makeList = function() {
+var makeList = function () {
   var MAX_NUMBER_X = 1200;
   var MIN_NUMBER_X = 0;
   var MAX_NUMBER_Y = 630;
@@ -22,13 +24,13 @@ var makeList = function() {
 
   for (var dataIndex = 0; dataIndex < 8; dataIndex++) {
     data[dataIndex] = {
-      "author": {
-        avatar: 'img/avatars/user0' + String(dataIndex+1) + '.png'
+      'author': {
+        avatar: 'img/avatars/user0' + String(dataIndex + 1) + '.png'
       },
-      "offer": {
-        type: livingType[getRandomNumber(0, livingType.length-1)]
+      'offer': {
+        type: livingType[getRandomNumber(0, livingType.length - 1)]
       },
-      "location": {
+      'location': {
         x: getRandomNumber(MIN_NUMBER_X, MAX_NUMBER_X),
         y: getRandomNumber(MIN_NUMBER_Y, MAX_NUMBER_Y)
       }
@@ -43,7 +45,7 @@ var makeList = function() {
  * @param {Object} template шаблон элемента
  * @return {Object} массив DOM-элементов
  */
-var createElement = function(elementList, template) {
+var createElement = function (elementList, template) {
   var PIN_WIDTH = 50;
   var PIN_HEIGHT = 70;
   var collection = [];
@@ -52,7 +54,7 @@ var createElement = function(elementList, template) {
     var pinModel = template.cloneNode(true);
 
     pinModel.querySelector('img').src = elementList[i].author.avatar;
-    pinModel.style.left = (elementList[i].location.x + PIN_WIDTH/2) + 'px';
+    pinModel.style.left = (elementList[i].location.x + PIN_WIDTH / 2) + 'px';
     pinModel.style.top = (elementList[i].location.y + PIN_HEIGHT) + 'px';
     collection[i] = pinModel;
   }
@@ -64,8 +66,8 @@ var createElement = function(elementList, template) {
  * @param {Object} collectionList массив DOM-элементов
  * @param {Object} block заполняемый блок
  */
-var fillBlock = function(collectionList, block) {
-  for (var i = 0; i<collectionList.length; i++) {
+var fillBlock = function (collectionList, block) {
+  for (var i = 0; i < collectionList.length; i++) {
     block.appendChild(collectionList[i]);
   }
 };
