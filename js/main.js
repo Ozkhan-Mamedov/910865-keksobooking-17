fd997f35fb9ee48702917a3bc1a78c01e1aefd3e
Ozkhan Mamedov <ozhanm96@yandex.ru>
2019-06-12 15:51:52 +0300

    Правки по комментариям

--- js/main.js
+++ js/main.js
@@ -1,6 +1,13 @@
 'use strict';
 
 /**
+ * Функция первоначальной настройки
+ */
+var setup = function () {
+  document.querySelector('.map').classList.remove('map--faded');
+};
+
+/**
  * Функция генерирования случайного числа
  * @param {number} min
  * @param {number} max
@@ -12,23 +19,25 @@ var getRandomNumber = function (min, max) {
 
 /**
  * Функция генерирования массива объектов, описывающих объявления
- * @return {Object} результат генерации
+ * @return {Object[]} результат генерации - массив объектов
  */
-var makeList = function () {
-  var MAX_NUMBER_X = 1200;
+var makeAdObjects = function () {
   var MIN_NUMBER_X = 0;
   var MAX_NUMBER_Y = 630;
   var MIN_NUMBER_Y = 130;
-  var data = [];
-  var livingType = ['palace', 'flat', 'house', 'bungalo'];
+  var MAX_NUMBER_X = document.querySelector('.map__pins').clientWidth;
+  var objects = [];
+  var livings = ['palace', 'flat', 'house', 'bungalo'];
+  var images = ['img/avatars/user01', 'img/avatars/user02', 'img/avatars/user03', 'img/avatars/user04', 'img/avatars/user05', 'img/avatars/user06', 'img/avatars/user07', 'img/avatars/user08'];
 
-  for (var dataIndex = 0; dataIndex < 8; dataIndex++) {
-    data[dataIndex] = {
+  for (var i = 0; i < 8; i++) {
+    objects[i] = {
       'author': {
-        avatar: 'img/avatars/user0' + String(dataIndex + 1) + '.png'
+        //avatar: 'img/avatars/user0' + String(i + 1) + '.png'
+        avatar: images[i] + '.png'
       },
       'offer': {
-        type: livingType[getRandomNumber(0, livingType.length - 1)]
+        type: livings[getRandomNumber(0, livings.length - 1)]
       },
       'location': {
         x: getRandomNumber(MIN_NUMBER_X, MAX_NUMBER_X),
@@ -36,45 +45,54 @@ var makeList = function () {
       }
     };
   }
-  return data;
+
+  return objects;
 };
 
 /**
  * Функция создания DOM-элементов
- * @param {Object} elementList массив объектов
+ * @param {Object[]} objects массив объектов
  * @param {Object} template шаблон элемента
- * @return {Object} массив DOM-элементов
+ * @return {Object[]} массив DOM-элементов
  */
-var createElement = function (elementList, template) {
+var createDomElements = function (objects, template) {
   var PIN_WIDTH = 50;
   var PIN_HEIGHT = 70;
-  var collection = [];
+  var elements = [];
 
-  for (var i = 0; i < elementList.length; i++) {
+  for (var i = 0; i < objects.length; i++) {
     var pinModel = template.cloneNode(true);
 
-    pinModel.querySelector('img').src = elementList[i].author.avatar;
-    pinModel.style.left = (elementList[i].location.x + PIN_WIDTH / 2) + 'px';
-    pinModel.style.top = (elementList[i].location.y + PIN_HEIGHT) + 'px';
-    collection[i] = pinModel;
+    pinModel.querySelector('img').src = objects[i].author.avatar;
+    pinModel.style.left = (objects[i].location.x - PIN_WIDTH / 2) + 'px';
+    pinModel.style.top = (objects[i].location.y - PIN_HEIGHT) + 'px';
+    elements[i] = pinModel;
   }
-  return collection;
+
+  return elements;
 };
 
 /**
  * Функция заполнения блока DOM-элементами
- * @param {Object} collectionList массив DOM-элементов
- * @param {Object} block заполняемый блок
+ * @param {Object[]} elements массив DOM-элементов
+ * @param {Object} block блок, заполняемый DOM-элементами
  */
-var fillBlock = function (collectionList, block) {
-  for (var i = 0; i < collectionList.length; i++) {
-    block.appendChild(collectionList[i]);
+var renderElements = function (elements, block) {
+  var nodes = document.createDocumentFragment();
+
+  for (var i = 0; i < elements.length; i++) {
+    nodes.appendChild(elements[i]);
   }
+
+  block.appendChild(nodes);
 };
+
+
+setup();
 
 var mapPins = document.querySelector('.map__pins');
 var template = document.querySelector('#pin').content.querySelector('.map__pin');
-var data = makeList();
-var pinCollection = createElement(data, template);
+var data = makeAdObjects();
+var pins = createDomElements(data, template);
 
-fillBlock(pinCollection, mapPins);
+renderElements(pins, mapPins);
