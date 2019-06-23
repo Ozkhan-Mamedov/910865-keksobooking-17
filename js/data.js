b13ff8f6f39173f44729710ce2680f545a951c97
Ozkhan Mamedov <ozhanm96@yandex.ru>
2019-06-23 05:55:59 +0300

    Задание 5.4 Разделение кода main.js на модули data, form, page-setup, pin, util

---
+++ js/data.js
@@ -0,0 +1,69 @@
+'use strict';
+
+(function () {
+  /**
+   * Функция генерирования массива объектов, описывающих объявления
+   * @return {Object[]} результат генерации - массив объектов
+   */
+  window.makeAdObjects = function () {
+    var objects = [];
+    var livings = ['palace', 'flat', 'house', 'bungalo'];
+    var images = ['img/avatars/user01', 'img/avatars/user02', 'img/avatars/user03', 'img/avatars/user04', 'img/avatars/user05', 'img/avatars/user06', 'img/avatars/user07', 'img/avatars/user08'];
+
+    for (var i = 0; i < 8; i++) {
+      objects[i] = {
+        'author': {
+          avatar: images[i] + '.png'
+        },
+        'offer': {
+          type: livings[window.util.getRandomNumber(0, livings.length - 1)]
+        },
+        'location': {
+          x: window.util.getRandomNumber(window.util.MIN_NUMBER_X, window.util.MAX_NUMBER_X),
+          y: window.util.getRandomNumber(window.util.MIN_NUMBER_Y, window.util.MAX_NUMBER_Y)
+        }
+      };
+    }
+
+    return objects;
+  };
+
+  /**
+   * Функция создания DOM-элементов
+   * @param {Object[]} objects массив объектов
+   * @param {Object} template шаблон элемента
+   * @return {Object[]} массив DOM-элементов
+   */
+  window.createDomElements = function (objects, template) {
+    var PIN_WIDTH = 50;
+    var PIN_HEIGHT = 70;
+    var elements = [];
+
+    for (var i = 0; i < objects.length; i++) {
+      var pinModel = template.cloneNode(true);
+
+      pinModel.querySelector('img').src = objects[i].author.avatar;
+      pinModel.style.left = (objects[i].location.x - PIN_WIDTH / 2) + 'px';
+      pinModel.style.top = (objects[i].location.y - PIN_HEIGHT) + 'px';
+      elements[i] = pinModel;
+    }
+
+    return elements;
+  };
+
+  /**
+   * Функция заполнения блока DOM-элементами
+   * @param {Object[]} elements массив DOM-элементов
+   * @param {Object} block блок, заполняемый DOM-элементами
+   */
+  window.renderElements = function (elements) {
+    var mapPins = document.querySelector('.map__pins');
+    var nodes = document.createDocumentFragment();
+
+    for (var i = 0; i < elements.length; i++) {
+      nodes.appendChild(elements[i]);
+    }
+
+    mapPins.appendChild(nodes);
+  };
+})();
---
+++ js/util.js
@@ -0,0 +1,66 @@
+'use strict';
+
+(function () {
+  var MIN_NUMBER_X = 0;
+  var MAX_NUMBER_Y = 630;
+  var MIN_NUMBER_Y = 130;
+  var MAX_NUMBER_X = document.querySelector('.map__pins').clientWidth;
+
+  /**
+   * Функция копирования элементов из псевдомассива в массив
+   * @param {Object[]} arr псевдомассив
+   * @return {Object[]} массив
+   */
+  var copyElements = function (arr) {
+    var arrModified = [];
+
+    for (var i = 0; i < arr.length; i++) {
+      arrModified[i] = arr[i];
+    }
+
+    return arrModified;
+  };
+
+  /**
+   * Функция генерирования случайного числа
+   * @param {Number} min
+   * @param {Number} max
+   * @return {Number} результат генерации
+   */
+  var getRandomNumber = function (min, max) {
+    return Math.round(Math.random() * (max - min) + min);
+  };
+
+  /**
+   * Функция извлекает число из строки
+   * @param {Object[]} data массив строк с координатами
+   * @return {Object[]} обработанный массив
+   */
+  var extractNumber = function (data) {
+    var numberX = parseFloat(data[0].substr(6));
+    var numberY = parseFloat(data[1].substr(6));
+
+    return [numberX, ' ' + numberY];
+  };
+
+  /**
+   * Функция получения координат
+   * @param {String} expression подаваемая строка
+   * @return {Object[]} массив с координатами
+   */
+  var getCoords = function (expression) {
+    var coords = expression.split(';');
+
+    return extractNumber(coords);
+  };
+
+  window.util = {
+    MIN_NUMBER_X: MIN_NUMBER_X,
+    MIN_NUMBER_Y: MIN_NUMBER_Y,
+    MAX_NUMBER_X: MAX_NUMBER_X,
+    MAX_NUMBER_Y: MAX_NUMBER_Y,
+    copyElements: copyElements,
+    getRandomNumber: getRandomNumber,
+    getCoords: getCoords
+  };
+})();
