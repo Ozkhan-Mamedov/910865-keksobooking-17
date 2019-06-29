3b09cab0e837f99968cade590a6e57591661c3e0
Ozkhan Mamedov <ozhanm96@yandex.ru>
2019-06-30 02:04:43 +0300

    Правки по комментариям

--- js/pin.js
+++ js/pin.js
@@ -4,20 +4,23 @@
   var MAIN_PIN_WIDTH = 65;
   var MAIN_PIN_HEIGHT = 65;
 
+  var util = window.util;
   var template = document.querySelector('#pin').content.querySelector('.map__pin');
   var mainPin = document.querySelector('.map__pin--main');
   window.addressInput = document.querySelector('#address');
 
   /**
    * Обработчик загрузки данных
-   * @param {Object[]} data
+   * @param { {author: string,
+   *           offer: string,
+   *           location: Object[]}[] } data
    */
   var onLoad = function (data) {
     var pins = window.createDomElements(data, template);
 
     mainPin.addEventListener('mouseup', function () {
       var pinCoordsStyle = mainPin.getAttribute('style');
-      var pinCoords = window.util.getCoords(pinCoordsStyle);
+      var pinCoords = util.getCoords(pinCoordsStyle);
       window.addressInput.setAttribute('value', pinCoords);
     });
     mainPin.addEventListener('mousedown', function (evt) {
@@ -26,41 +29,9 @@
         window.renderElements(pins);
       }
 
-      var startCoords = {
+      window.startCoords = {
         x: evt.clientX,
         y: evt.clientY
-      };
-
-      /**
-       * Обработчик перемещения мыши
-       * @param {MouseEvent} moveEvt объект события перемещения мыши
-       */
-      var onMouseMove = function (moveEvt) {
-        var shift = {
-          x: startCoords.x - moveEvt.clientX,
-          y: startCoords.y - moveEvt.clientY
-        };
-
-        startCoords = {
-          x: moveEvt.clientX,
-          y: moveEvt.clientY
-        };
-
-        mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
-        mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
-
-        if (mainPin.offsetLeft < window.util.MIN_NUMBER_X - MAIN_PIN_WIDTH / 2) {
-          mainPin.style.left = window.util.MIN_NUMBER_X - MAIN_PIN_WIDTH / 2 + 'px';
-        }
-        if (mainPin.offsetLeft > window.util.MAX_NUMBER_X - MAIN_PIN_WIDTH / 2) {
-          mainPin.style.left = window.util.MAX_NUMBER_X - MAIN_PIN_WIDTH / 2 + 'px';
-        }
-        if (mainPin.offsetTop < window.util.MIN_NUMBER_Y - MAIN_PIN_HEIGHT) {
-          mainPin.style.top = window.util.MIN_NUMBER_Y - MAIN_PIN_HEIGHT + 'px';
-        }
-        if (mainPin.offsetTop > window.util.MAX_NUMBER_Y) {
-          mainPin.style.top = window.util.MAX_NUMBER_Y + 'px';
-        }
       };
 
       /**
@@ -77,10 +48,42 @@
   };
 
   /**
+   * Обработчик перемещения мыши
+   * @param {MouseEvent} moveEvt объект события перемещения мыши
+   */
+  var onMouseMove = function (moveEvt) {
+    var shift = {
+      x: window.startCoords.x - moveEvt.clientX,
+      y: window.startCoords.y - moveEvt.clientY
+    };
+
+    window.startCoords = {
+      x: moveEvt.clientX,
+      y: moveEvt.clientY
+    };
+
+    mainPin.style.top = (mainPin.offsetTop - shift.y) + 'px';
+    mainPin.style.left = (mainPin.offsetLeft - shift.x) + 'px';
+
+    if (mainPin.offsetLeft < util.MIN_NUMBER_X - MAIN_PIN_WIDTH / 2) {
+      mainPin.style.left = util.MIN_NUMBER_X - MAIN_PIN_WIDTH / 2 + 'px';
+    }
+    if (mainPin.offsetLeft > util.MAX_NUMBER_X - MAIN_PIN_WIDTH / 2) {
+      mainPin.style.left = util.MAX_NUMBER_X - MAIN_PIN_WIDTH / 2 + 'px';
+    }
+    if (mainPin.offsetTop < util.MIN_NUMBER_Y - MAIN_PIN_HEIGHT) {
+      mainPin.style.top = util.MIN_NUMBER_Y - MAIN_PIN_HEIGHT + 'px';
+    }
+    if (mainPin.offsetTop > util.MAX_NUMBER_Y) {
+      mainPin.style.top = util.MAX_NUMBER_Y + 'px';
+    }
+  };
+
+  /**
    * Обработчик ошибки
    */
   var onError = function () {
-    window.util.generateErrorMessage();
+    util.generateErrorMessage();
   };
 
   window.load(onLoad, onError);
